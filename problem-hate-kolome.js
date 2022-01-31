707df985fa55608296fe07f90677eaf516613870
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-01 00:35:35 +0600

    solve

--- problem-hate-kolome.js
+++ problem-hate-kolome.js
@@ -1,6 +1,21 @@
 // 1. ৩ টা নাম্বার থেকে বড় নাম্বার খুঁজে বের করবেন।  
+// Standard  
+// Largest number 
+function largestNUmber (num1, num2, num3){
+    let result = Math.max(num1, num2, num3) ;
+    // console.log('Largest number is :' , result);
+    return result ;
+}
+let num1 = 10
+let num2 = 20
+let num3 = 30 
+let largeNumber = largestNUmber(num1, num2, num3)
+console.log('Largest number is :' , largeNumber); 
 
-function largestNumber (num1 ,num2 ,num3 ){
+
+// if else  
+
+/* function largestNumber (num1 ,num2 ,num3 ){
     if(num1 > num2 && num1 > num3){
         // console.log('Largest number is ' + num1);
         return num1
@@ -19,4 +34,4 @@ var num1 = 00
 var num2 = 20 
 var num3 = 14 
 var bigNumber = largestNumber(num1 , num2 ,num3 ) 
-console.log('The learge number is :' , bigNumber);
+console.log('The large number is :' , bigNumber); */
