5cdc39fe293231d6b4e893efdcad20143158bc93
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-07-07 22:06:00 +0600

    sqrt concept

--- Daily Problem Solve/introduction.js
+++ Daily Problem Solve/introduction.js
@@ -1,1 +1,10 @@
 //JavaScript Program to Find the Square Root
+let num = 9
+console.log(Math.sqrt(num));
+
+let num2 = 'Emon'
+console.log(Math.sqrt(num2));
+
+//If 0 or a positive number is passed in the Math.sqrt() method, then the square root of that number is returned.
+//If a negative number is passed, NaN is returned.
+//If a string is passed, NaN is returned. 
