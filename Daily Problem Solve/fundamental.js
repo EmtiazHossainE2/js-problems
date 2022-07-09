30a670695dc046c2e3c49018960db0c5ce4e9048
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-07-10 00:20:18 +0600

    fundamental concept

---
+++ Daily Problem Solve/fundamental.js
@@ -0,0 +1,19 @@
+//1.Remove first n characters of string
+function myFunc(a) {
+    // return a.slice(3, 5)
+    return a.substring(3, 7) //3 num e jeta oita diye start hobe,r 7 num theke bad sob 
+}
+
+console.log(myFunc('Emtiazfdf'));
+
+//2.Get first n characters of string
+function getStr(str) {
+    return str.slice(0, 3)
+}
+console.log(getStr('123456'));
+
+//3.Extract first half of string
+function halfStr(str) {
+    return
+}
+console.log(halfStr('Emtiaz'));
