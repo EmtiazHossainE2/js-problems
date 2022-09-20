979996e594f31b05d6b8961cb283dfb5ac779613
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-09-20 20:51:28 +0600

    array challenge

--- listed-problem.js
+++ listed-problem.js
@@ -128,3 +128,19 @@ function MathChallenge(num) {
   }
   
   console.log(MathChallenge(65532));
+
+  // 7 Have the function ArrayChallenge(arr) take the array of integers stored in arr, and determine if any two numbers (excluding the first element) in the array can sum up to the first element in the array.  For example: if arr is [7, 3, 5, 2, -4, 8, 11], then there are actually two pairs that sum to the number 7: [5, 2] and [-4, 11]. Your program should return all pairs, with the numbers separated by a comma, in the order the first number appears in the array. Pairs should be separated by a space. So for the example above, your program would return: 5,2 -4,11 If there are no two numbers that sum to the first element in the array, return -1 
+
+  function ArrayChallenge(arr) {
+    let sum = []
+    for (let i = 1; i < arr.length; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] + arr[j] === arr[0]) {
+          sum.push([arr[i], arr[j]].join());
+        }
+      }
+    }
+    return sum.length > 0 ? sum.join(" ") : -1;
+  }
+  const input = [17, 4, 5, 6, 10, 11, 4, -3, -5, 3, 15, 2, 7]
+  console.log(ArrayChallenge(input));
